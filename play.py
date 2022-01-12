f932a9f4ffdd69803428bba78c18b284af065ba5
ariffrahimin <57780003+ariffrahimin@users.noreply.github.com>
2022-01-12 14:32:17 +0800

    first commit

---
+++ play.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+def conjecture(x):
+    thisarray = []
+    thisarray.append(x)
+    steps = 0
+    while x != 1:
+        if (x%2) == 0:
+            x = x/2   
+            thisarray.append(x)
+            steps = steps + 1
+        else:
+            x = 3 * x + 1           
+            thisarray.append(x)
+            steps = steps + 1
+    print("total steps: ",steps)
+    plt.plot(thisarray)
+    plt.show()
+    
+
+if __name__ == "__main__":
+    x = int(input())
+    
+    conjecture(x)
